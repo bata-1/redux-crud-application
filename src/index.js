1193d70fed829f7a748980c325ce92d4f4138933
sharing-creator <batahiromk2@gmail.com>
2020-12-23 21:25:27 +0900

    implement component with connectactions

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import { Provider } from 'react-redux'
 
 import './index.css';
 import reducer from './reducers'
-import App from './component/App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer)
 
@@ -15,4 +15,4 @@ ReactDOM.render(
   <App />
 </Provider>, 
   document.getElementById('root'));
-registerServiceWorker();
+  reportWebVitals();
